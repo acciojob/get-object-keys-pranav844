15fb888011d8f787c26c6ec43ae1a602ed052938
PRANAV SAXENA <134140979+pranav844@users.noreply.github.com>
2024-06-10 13:39:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let student = {
+    name: "John Doe"
+};
+
+// Adding the getKeys method to the Object prototype
+Object.prototype.getKeys = function() {
+    return Object.keys(this);
+};
+
+// Testing the getKeys method
+console.log(student.getKeys()); // Output: ["name"]
